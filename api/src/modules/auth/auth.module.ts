66316cf200895f15be561a25fafeb4c8edd1a840
Tran Dinh Quy <124875024+dqez@users.noreply.github.com>
2026-06-24 23:57:42 +0700

    feat: add login endpoint

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import type { StringValue } from 'ms';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: config.getOrThrow<string>('JWT_EXPIRES_IN') as StringValue,
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,11 +1,21 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
 
   async register(dto: RegisterDto) {
     const emailExist = await this.prisma.user.findUnique({
@@ -31,5 +41,50 @@ export class AuthService {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password_hash, ...newUserWithoutPassword } = newUser;
     return newUserWithoutPassword;
+  }
+
+  async login(dto: LoginDto) {
+    const userExist = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!userExist) {
+      throw new UnauthorizedException('The email or password was wrong!');
+    }
+    if (!userExist.password_hash) {
+      throw new UnauthorizedException('The email or password was wrong!');
+    }
+
+    const result = await bcrypt.compare(dto.password, userExist.password_hash);
+    if (!result) {
+      throw new UnauthorizedException('The email or password was wrong!');
+    }
+
+    const payload = {
+      sub: userExist.id,
+      email: userExist.email,
+      role: userExist.role,
+    };
+
+    const accessToken = this.jwt.sign(payload);
+
+    const refreshToken = randomUUID();
+    const refreshTokenHash = await bcrypt.hash(refreshToken, 10);
+    const familyId = randomUUID();
+
+    await this.prisma.refreshToken.create({
+      data: {
+        user_id: userExist.id,
+        token_hash: refreshTokenHash,
+        family_id: familyId,
+        expires_at: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //7 days
+      },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password_hash, ...user } = userExist;
+    return { user, accessToken, refreshToken };
   }
 }
